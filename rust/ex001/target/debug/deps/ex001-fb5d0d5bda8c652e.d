efc9f4e22674c29e73a10199d5fe92f965da3072
mailsonpuc <mailson.costa@proton.me>
2024-12-11 22:24:35 -0300

    rust

---
+++ rust/ex001/target/debug/deps/ex001-fb5d0d5bda8c652e.d
@@ -0,0 +1,5 @@
+/home/masv/AlgoritmosExercicios/rust/ex001/target/debug/deps/ex001-fb5d0d5bda8c652e: src/main.rs
+
+/home/masv/AlgoritmosExercicios/rust/ex001/target/debug/deps/ex001-fb5d0d5bda8c652e.d: src/main.rs
+
+src/main.rs:
---
+++ rust/ex001/target/debug/ex001.d
@@ -0,0 +1,1 @@
+/home/masv/AlgoritmosExercicios/rust/ex001/target/debug/ex001: /home/masv/AlgoritmosExercicios/rust/ex001/src/main.rs
